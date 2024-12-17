dd770912f7d10b27dddc4fc7c0dce0e1d6fc1a4f
moosa-raheel <moosarahil13@gmail.com>
2024-12-17 14:00:41 -0500

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import { Home } from "./pages";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
